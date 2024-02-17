55a622548a32a736fc3d68f40f090b5c38b94119
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-02-17 16:50:48 +0200

    Remove unnecessary import statements in LoggingAspect.java and add a missing import in SectorController

--- src/main/java/com/helmes/recruitment/formhandler/configuration/aspect/LoggingAspect.java
+++ src/main/java/com/helmes/recruitment/formhandler/configuration/aspect/LoggingAspect.java
@@ -7,8 +7,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
