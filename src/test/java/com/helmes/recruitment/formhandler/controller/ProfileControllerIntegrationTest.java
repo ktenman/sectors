d44ef00ed9159ed844da57670a8c5b4e7e5664d6
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-02-18 11:21:23 +0200

    Add profile update integration test

--- src/test/java/com/helmes/recruitment/formhandler/controller/ProfileControllerIntegrationTest.java
+++ src/test/java/com/helmes/recruitment/formhandler/controller/ProfileControllerIntegrationTest.java
@@ -6,10 +6,11 @@ import com.helmes.recruitment.formhandler.domain.Profile;
 import com.helmes.recruitment.formhandler.domain.Sector;
 import com.helmes.recruitment.formhandler.models.CreateProfileRequest;
 import com.helmes.recruitment.formhandler.models.ProfileDTO;
+import com.helmes.recruitment.formhandler.repository.ProfileRepository;
 import com.helmes.recruitment.formhandler.service.SessionService;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -21,6 +22,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ProfileControllerIntegrationTest {
 	
 	private static final String DEFAULT_NAME = "John Doe";
+	private static final UUID DEFAULT_SESSION_ID = UUID.randomUUID();
 	
 	@Autowired
 	private MockMvc mockMvc;
@@ -39,14 +42,19 @@ class ProfileControllerIntegrationTest {
 	@Autowired
 	private EntityManager entityManager;
 	
+	@Autowired
+	private ProfileRepository profileRepository;
+	
 	@MockBean
 	private SessionService sessionService;
 	
+	@BeforeEach
+	void setUp() {
+		when(sessionService.getSession()).thenReturn(DEFAULT_SESSION_ID);
+	}
+	
 	@Test
-	void whenSaveProfile_thenReturnsSavedProfile() throws Exception {
-		UUID sessionId = UUID.randomUUID();
-		Mockito.when(sessionService.getSession()).thenReturn(sessionId);
-		
+	void saveProfile_givenValidProfileData_thenReturnsSavedProfile() throws Exception {
 		CreateProfileRequest createProfileRequest = CreateProfileRequest.builder()
 				.name(DEFAULT_NAME)
 				.agreeToTerms(true)
@@ -70,13 +78,59 @@ class ProfileControllerIntegrationTest {
 				.satisfies(p -> {
 					assertThat(p.getName()).isEqualTo(DEFAULT_NAME);
 					assertThat(p.getAgreeToTerms()).isTrue();
-					assertThat(p.getSessionId()).isEqualTo(sessionId);
+					assertThat(p.getSessionId()).isEqualTo(DEFAULT_SESSION_ID);
 					assertThat(p.getSectors())
 							.hasSize(2)
 							.extracting(Sector::getId, Sector::getName)
 							.containsExactlyInAnyOrder(
 									tuple(2L, "Service"),
 									tuple(22L, "Tourism")
+							);
+					assertThat(p.getCreatedAt()).isNotNull().isBefore(now);
+					assertThat(p.getUpdatedAt()).isNotNull().isBefore(now);
+				});
+	}
+	
+	@Test
+	void updateProfile_givenValidData_thenReturnsUpdatedProfile() throws Exception {
+		Profile profile = new Profile();
+		profile.setSessionId(DEFAULT_SESSION_ID);
+		profile.setName("Old Name");
+		profile.setAgreeToTerms(true);
+		profileRepository.save(profile);
+		
+		List<Long> sectors = List.of(1L, 2L, 8L);
+		CreateProfileRequest createProfileRequest = CreateProfileRequest.builder()
+				.name(DEFAULT_NAME)
+				.agreeToTerms(true)
+				.sectors(sectors)
+				.build();
+		ProfileDTO expectedResponse = new ProfileDTO(1L, DEFAULT_NAME, true, sectors);
+		
+		mockMvc.perform(post("/api/profiles")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(createProfileRequest)))
+				.andExpect(status().isOk())
+				.andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)));
+		
+		List<Profile> profiles = entityManager.createQuery(
+						"SELECT p FROM Profile p JOIN FETCH p.sectors", Profile.class)
+				.getResultList();
+		
+		Instant now = Instant.now();
+		assertThat(profiles).isNotEmpty()
+				.first()
+				.satisfies(p -> {
+					assertThat(p.getName()).isEqualTo(DEFAULT_NAME);
+					assertThat(p.getAgreeToTerms()).isTrue();
+					assertThat(p.getSessionId()).isEqualTo(DEFAULT_SESSION_ID);
+					assertThat(p.getSectors())
+							.hasSize(3)
+							.extracting(Sector::getId, Sector::getName)
+							.containsExactlyInAnyOrder(
+									tuple(2L, "Service"),
+									tuple(8L, "Wood"),
+									tuple(1L, "Manufacturing")
 							);
 					assertThat(p.getCreatedAt()).isNotNull().isBefore(now);
 					assertThat(p.getUpdatedAt()).isNotNull().isBefore(now);
