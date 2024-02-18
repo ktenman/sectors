d33dca3a76bb67e0a11c6140f2ef6cbee965582b
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-02-18 10:44:55 +0200

    Remove @Slf4j annotation from SectorController class

--- src/main/java/com/helmes/recruitment/formhandler/controller/SectorController.java
+++ src/main/java/com/helmes/recruitment/formhandler/controller/SectorController.java
@@ -4,7 +4,6 @@ import com.helmes.recruitment.formhandler.configuration.logging.aspect.Loggable;
 import com.helmes.recruitment.formhandler.models.SectorDTO;
 import com.helmes.recruitment.formhandler.service.SectorService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/sectors")
 @RequiredArgsConstructor
-@Slf4j
 public class SectorController {
 	
 	private final SectorService sectorService;
