02df6e0224c399a2af5c792a8c559a1f4d0872c9
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-02-18 20:38:40 +0200

    Add DBHelperService dependency and methods for interacting with database

--- src/test/java/com/helmes/recruitment/formhandler/controller/ProfileControllerIntegrationTest.java
+++ src/test/java/com/helmes/recruitment/formhandler/controller/ProfileControllerIntegrationTest.java
@@ -1,15 +1,14 @@
 package com.helmes.recruitment.formhandler.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.helmes.recruitment.formhandler.DBHelperService;
 import com.helmes.recruitment.formhandler.IntegrationTest;
 import com.helmes.recruitment.formhandler.configuration.exception.GlobalExceptionHandler.ApiError;
 import com.helmes.recruitment.formhandler.domain.Profile;
 import com.helmes.recruitment.formhandler.domain.Sector;
 import com.helmes.recruitment.formhandler.models.CreateProfileRequest;
 import com.helmes.recruitment.formhandler.models.ProfileDTO;
-import com.helmes.recruitment.formhandler.repository.ProfileRepository;
 import com.helmes.recruitment.formhandler.service.SessionService;
-import jakarta.persistence.EntityManager;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,15 +17,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,7 +38,7 @@ class ProfileControllerIntegrationTest {
 	
 	private static final String DEFAULT_NAME = "John Doe";
 	private static final String LONG_NAME_65_CHARS = RandomStringUtils.randomAlphabetic(65);
-	private static final UUID DEFAULT_SESSION_ID = UUID.randomUUID();
+	private static final UUID DEFAULT_SESSION_ID = UUID.fromString("80ccee8e-c7ef-4a66-9871-a399b3870132");
 	
 	@Autowired
 	private MockMvc mockMvc;
@@ -45,10 +47,7 @@ class ProfileControllerIntegrationTest {
 	private ObjectMapper objectMapper;
 	
 	@Autowired
-	private EntityManager entityManager;
-	
-	@Autowired
-	private ProfileRepository profileRepository;
+	private DBHelperService dbHelperService;
 	
 	@MockBean
 	private SessionService sessionService;
@@ -73,9 +72,7 @@ class ProfileControllerIntegrationTest {
 				.andExpect(status().isCreated())
 				.andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)));
 		
-		List<Profile> profiles = entityManager.createQuery(
-						"SELECT p FROM Profile p JOIN FETCH p.sectors", Profile.class)
-				.getResultList();
+		List<Profile> profiles = dbHelperService.getProfiles();
 		
 		Instant now = Instant.now();
 		assertThat(profiles).isNotEmpty()
@@ -98,11 +95,7 @@ class ProfileControllerIntegrationTest {
 	
 	@Test
 	void updateProfile_givenValidData_thenReturnsUpdatedProfile() throws Exception {
-		Profile profile = new Profile();
-		profile.setSessionId(DEFAULT_SESSION_ID);
-		profile.setName("Old Name");
-		profile.setAgreeToTerms(true);
-		profileRepository.save(profile);
+		dbHelperService.persist(aProfile());
 		
 		List<Long> sectors = List.of(1L, 2L, 8L);
 		CreateProfileRequest createProfileRequest = CreateProfileRequest.builder()
@@ -118,9 +111,7 @@ class ProfileControllerIntegrationTest {
 				.andExpect(status().isOk())
 				.andExpect(content().json(objectMapper.writeValueAsString(expectedResponse)));
 		
-		List<Profile> profiles = entityManager.createQuery(
-						"SELECT p FROM Profile p JOIN FETCH p.sectors", Profile.class)
-				.getResultList();
+		List<Profile> profiles = dbHelperService.getProfiles();
 		
 		Instant now = Instant.now();
 		assertThat(profiles).isNotEmpty()
@@ -141,15 +132,16 @@ class ProfileControllerIntegrationTest {
 					assertThat(p.getUpdatedAt()).isNotNull().isBefore(now);
 				});
 	}
-	
+
 	@Test
 	void saveProfile_withInvalidProfileData_returnsMultipleValidationErrors() throws Exception {
-		ResultActions resultActions = mockMvc.perform(post("/api/profiles")
+		MvcResult mvcResult = mockMvc.perform(post("/api/profiles")
 						.contentType(MediaType.APPLICATION_JSON)
 						.content("{}"))
-				.andExpect(status().isBadRequest());
+				.andExpect(status().isBadRequest())
+				.andReturn();
 		
-		ApiError apiError = objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(), ApiError.class);
+		ApiError apiError = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ApiError.class);
 		assertThat(apiError.getMessage()).isEqualTo("Validation error");
 		assertThat(apiError.getDebugMessage()).isEqualTo("One or more fields have an error");
 		assertThat(apiError.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
@@ -157,6 +149,20 @@ class ProfileControllerIntegrationTest {
 				.containsEntry("name", "Name is required")
 				.containsEntry("agreeToTerms", "Agreement to terms is mandatory")
 				.containsEntry("sectors", "At least one sector must be selected");
+	}
+	
+	@Test
+	void saveProfile_withMissingRequestBody_throwsInternalServerError() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(post("/api/profiles")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isInternalServerError())
+				.andReturn();
+		
+		ApiError apiError = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ApiError.class);
+		assertThat(apiError.getMessage()).contains("Required request body is missing");
+		assertThat(apiError.getDebugMessage()).isEqualTo("An internal error occurred");
+		assertThat(apiError.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+		assertThat(apiError.getValidationErrors()).isNull();
 	}
 	
 	@Test
@@ -181,5 +187,49 @@ class ProfileControllerIntegrationTest {
 				.containsValue("Name must not exceed 64 characters");
 	}
 	
+	@Test
+	void requestProfile_whenProfileNotFound_shouldReturnForbiddenStatus() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(get("/api/profiles")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden())
+				.andReturn();
+		
+		ApiError apiError = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ApiError.class);
+		assertThat(apiError.getMessage()).isEqualTo("Profile not found for sessionId: 80ccee8e-c7ef-4a66-9871-a399b3870132");
+		assertThat(apiError.getDebugMessage()).isEqualTo("Access to the requested resource is denied");
+		assertThat(apiError.getStatus()).isEqualTo(HttpStatus.FORBIDDEN);
+		assertThat(apiError.getValidationErrors()).isNull();
+	}
+	
+	@Test
+	void retrieveProfile_whenProfileExists_shouldReturnProfileDetailsSuccessfully() throws Exception {
+		Profile profile = aProfile();
+		dbHelperService.persist(profile);
+		
+		MvcResult mvcResult = mockMvc.perform(get("/api/profiles")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		ProfileDTO profileDTO = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), ProfileDTO.class);
+		assertThat(profileDTO.id()).isEqualTo(1L);
+		assertThat(profileDTO.name()).isEqualTo("Old Name");
+		assertThat(profileDTO.agreeToTerms()).isTrue();
+		Long expectedSectorId = profile.getSectors().stream().map(Sector::getId).findFirst().orElseThrow();
+		assertThat(profileDTO.sectors()).singleElement().isEqualTo(expectedSectorId);
+	}
+	
+	private Profile aProfile() {
+		Profile profile = new Profile();
+		profile.setSessionId(DEFAULT_SESSION_ID);
+		profile.setName("Old Name");
+		profile.setAgreeToTerms(true);
+		
+		Sector sector = new Sector();
+		sector.setName("Cars Manufacturing");
+		profile.setSectors(Set.of(sector));
+		
+		return profile;
+	}
 	
 }
