905c453ac7da2f8b2fe3bea1ab566b54fc8b0a73
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-03-06 19:46:43 +0200

    Update default name variable to improve code readability

--- src/test/java/e2e/SubmitFormE2ETest.java
+++ src/test/java/e2e/SubmitFormE2ETest.java
@@ -14,11 +14,13 @@ import static org.openqa.selenium.By.id;
 
 class SubmitFormE2ETest {
 	
+	private static final String DEFAULT_NAME = "Konstantin";
+	
 	@Test
 	void createProfile() {
 		open("http://localhost:61234");
 		
-		$(id("name")).shouldNotHave(text("Konstantin")).setValue("Konstantin");
+		$(id("name")).shouldNotHave(text(DEFAULT_NAME)).setValue(DEFAULT_NAME);
 		ElementsCollection sectors = $(id("sectors")).getOptions();
 		sectors.find(text("Construction materials")).click();
 		sectors.find(text("Beverages")).click();
@@ -29,7 +31,7 @@ class SubmitFormE2ETest {
 		assertThat(sectors).hasSize(79);
 		Selenide.refresh();
 		$(id("formAlert")).shouldNotBe(visible);
-		assertThat($(By.id("name")).val()).isEqualTo("Konstantin");
+		assertThat($(By.id("name")).val()).isEqualTo(DEFAULT_NAME);
 	}
 	
 }
