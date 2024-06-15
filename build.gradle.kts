4738cd2e812da0b0fcd920a8ebb795a2622714d8
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-06-15 19:16:55 +0300

    Update Spring Flyway dependency to use PostgreSQL database

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.5"
+    id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
     id("jacoco")
     id("org.sonarqube") version "5.0.0.4638"
@@ -13,6 +13,9 @@ val selenideVersion = "7.3.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
@@ -37,7 +40,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.springframework.session:spring-session-data-redis")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocOpenApiVersion")
     compileOnly("org.projectlombok:lombok")
